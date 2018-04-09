9614542840bdaace8023b96871fd7ed594ffa091
you2xr@126.com <you2xr@126.com>
2018-04-09 14:38:33 +0800

    set webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
+const isDev = process.env.NODE_NEV === 'development'
+
+const config = {
+    target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle-[hash].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -28,7 +33,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                test: /\.(gif|jpg|jpeg|png|svg|ico)$/,
                 use: [
                     {
                         loader: 'url-loader',
@@ -40,5 +45,33 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
 }
+
+if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8081,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true,
+        },
+        hot: true
+        // historyFallBack: {},
+        // open: true
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config
